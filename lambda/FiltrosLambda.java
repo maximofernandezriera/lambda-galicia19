fe35e793cf1b5134d32fe8d3536b3c2fab33013a
Máximo Fernández Riera <43608040+maximofdezriera@users.noreply.github.com>
2021-06-08 16:30:20 +0200

    Update FiltrosLambda.java

--- lambda/FiltrosLambda.java
+++ lambda/FiltrosLambda.java
@@ -1,18 +1,18 @@
 import java.util.function.Predicate;
 
 class FiltrosLambda {
- public static void main(String[] args) {
- String[] listaPalabras = {"cola", "limbo", "zapatos", "imposible"};
- System.out.print("Imprimir palabras con más de 5 caracteres: ");
- Predicate<String>predicado = s->s.length() > 5;
- filtrar(listaPalabras, predicado);
- System.out.print("\nImprimir palabras menores que vaca: ");
- filtrar(listaPalabras, s->s.compareTo("vaca") < 0);
- }
- //Método filtrar()
-public static void filtrar(String[] listaPalabras, Predicate<String> predicado){
+  public static void main(String[] args) {
+    String[] listaPalabras = {"cola", "limbo", "zapatos", "imposible"};
+    System.out.print("Imprimir palabras con más de 5 caracteres: ");
+    Predicate<String>predicado = s->s.length() > 5;
+    filtrar(listaPalabras, predicado);
+    System.out.print("\nImprimir palabras menores que vaca: ");
+    filtrar(listaPalabras, s->s.compareTo("vaca") < 0);
+  }
+  //Escribimos el método filtrar()
+  public static void filtrar(String[] listaPalabras, Predicate<String> predicado){
           for (String myString : listaPalabras)
                   if (predicado.test(myString))
                           System.out.print(myString+' ');
-          System.out.println("");
+  }
 }
